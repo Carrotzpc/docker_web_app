96ffab4b013f31b370652becb505bcf8bc15d800
Luobo Zhang <zhang.pc3@gmail.com>
2016-08-19 14:13:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
   let datenow = new Date()
   datenow = `${datenow.toLocaleDateString()} ${datenow.toLocaleTimeString()}`
   console.log(datenow, 'Hello world')
-  res.send('Hello world\n v2.0')
+  res.send('Hello world\n v2.2')
 })
 
 app.listen(PORT)
